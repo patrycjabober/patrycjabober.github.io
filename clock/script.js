67670cb05aa624a964a774b3b36df46da9c7bb7c
patrycjabober <43049082+patrycjabober@users.noreply.github.com>
2019-03-11 14:55:13 -0400

    Add files via upload

--- clock/script.js
+++ clock/script.js
@@ -4,11 +4,22 @@ $(document).ready(function(){
   setTimeout(function(){
     $(".red").addClass("alarm"); 
     $(".dot").addClass("alarm");
-  }, 30000);
+  }, 20000);
 
-   $(".red").click(function() { 
-    $(".red").removeClass("alarm");
-    $(".dot").removeClass("alarm");
+   $(".screen").click(function() { 
+    $(".screen").addClass("snooze");
+   	setTimeout(function(){
+   		$(".grow").addClass("snooze");
+   	},1000);
+    setTimeout(function(){
+   		$(".red").removeClass("alarm");
+    	$(".dot").removeClass("alarm");
+    	
+    },2000);
+    setTimeout(function() {
+    	$(".screen").removeClass("snooze");
+    	$(".dot").css("animation-name", "").delay(1500).css("animation-name", "animationC");
+    },5000);
   });
 
 }); 
