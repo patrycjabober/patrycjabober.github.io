8b31425ba2b7981e73c762af6901d556de52d28a
patrycjabober <43049082+patrycjabober@users.noreply.github.com>
2019-04-01 17:46:35 -0400

    Add files via upload

--- sprite/sprite/script.js
+++ sprite/sprite/script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
- });
+  mugen.initialize({ titleScreenImageURL: "titlescreen.gif",
+  	attackRadius: 100, monsterHitRadius: 100, attackDuration:600, 
+  	jumpHeight: 300, monsterMoveToX: 100, maxSimultaneousMonsters: 1 
+	});
+});
